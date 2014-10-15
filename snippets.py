d1c71cfd454c221ab60c3f722fdbaced33f58f22
Michael Nickey <mnickey@gmail.com>
2014-10-14 20:38:41 -0700

    created parser

--- snippets.py
+++ snippets.py
@@ -1,23 +1,35 @@
 import logging
 import csv
+import argparse
+import sys
 
 # Set the log output file, and the log level
-"""
-DEBUG: Detailed information, typically of interest only when diagnosing problems.
-INFO: Confirmation that things are working as expected.
-WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
-ERROR: Due to a more serious problem, the software has not been able to perform some function.
-CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
-"""
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
 def put(name, snippet, filename):
 	""" Store a snippet with an associated name in the CSV file """
 	logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
 	logging.debug("Opening file")
-	with open(filename, "a") as "f":
+	with open(filename, "a") as f:
 		writer = csv.writer(f)
 		logging.debug("Writting snippet to file...")
 		writer.writerow([name, snippet])
 	logging.debug("Write Sucessful")
 	return name, snippet
+
+def make_parser():
+	""" Construct a command line parser """
+	logging.info("Constructing parser")
+	description = "Store and retrieve snippets of text"
+	parser = argparse.ArgumentParser(description=description)
+
+	return parser
+
+def main():
+	""" Main function """
+	logging.info("Starting Snippets")
+	parser = make_parser()
+	arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == '__main__':
+	main()
