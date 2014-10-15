b99745278db6b392ecaa4825a2ab4f0732fab19d
Michael Nickey <mnickey@gmail.com>
2014-10-14 20:18:30 -0700

    updated logging script

--- snippets.py
+++ snippets.py
@@ -2,4 +2,22 @@ import logging
 import csv
 
 # Set the log output file, and the log level
+"""
+DEBUG: Detailed information, typically of interest only when diagnosing problems.
+INFO: Confirmation that things are working as expected.
+WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+ERROR: Due to a more serious problem, the software has not been able to perform some function.
+CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+"""
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
+
+def put(name, snippet, filename):
+	""" Store a snippet with an associated name in the CSV file """
+	logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
+	logging.debug("Opening file")
+	with open(filename, "a") as "f":
+		writer = csv.writer(f)
+		logging.debug("Writting snippet to file...")
+		writer.writerow([name, snippet])
+	logging.debug("Write Sucessful")
+	return name, snippet
